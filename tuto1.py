58e3251798d9b75f52d016da127a2d05bd539394
fatihahamani27 <s22b0013@siswa.umk.edu.my>
2024-10-27 11:08:19 +0800

    tuto1.py

--- tuto1.py
+++ tuto1.py
@@ -1,6 +1,4 @@
-import streamlit as st
-st.set_page_config
-(
-    page_title="Genetic Algorithm"
-)
+import streamlit as st  
+st.set_page_config(     
+page_title="Genetic Algorithm" )  
 st.header("Genetic Algorithm", divider="gray") 
